96ee541c0603073229aa7fc046794e1b6875f28e
asdrmada <42623227+asdrmada@users.noreply.github.com>
2019-01-19 18:07:41 +0000

    regstration form  sucsessfully adding too database

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express"),
-      router  = express.Router();
+      router  = express.Router(),
+      User    = require("../models/user");
 
 router.get("/login", (req, res) => res.render("auth/login"));
   
@@ -8,10 +9,28 @@ router.get("/register", (req, res) => res.render("auth/register"));
 router.post("/register", (req, res) => {
       let username = req.body.username,
           password = req.body.password;
-      
-      const newUser = {username : username, password : password};
-      console.log(newUser);
-    res.redirect("/");
+
+      User.findOne({username : username})
+      .then(user => {
+        if(user){
+            res.redirect('back');
+            alert("User already exists!!");
+        } else {
+        
+            const newUser = {username : username, password : password};
+            console.log(newUser);
+
+            User.create(newUser, function(err, newlyCreated){
+                if(err){
+                    res.redirect(back);
+                    console.log(err);
+                } else {
+                    res.redirect("login");
+                    console.log("User succsesfully created!");
+                }
+            });
+        }
+    });
 });
   
 
